e169c04d9c0c285e7aed242c8b535b4c7f4a7fcd
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-10-15 13:22:25 +0800

    Filter frequency response experiment: add V2 with batched FFTs
    It's bottlenecked on visualization and not on signal processing so there was little point

--- src/experiments/filter_frequency_response.py
+++ src/experiments/filter_frequency_response.py
@@ -114,3 +114,109 @@ def run_v1():
 	disp = page([spectral])
 	disp.show()
 	input()
+
+def run_v2():
+	frames = 8192
+	rate = 5*1000*1000
+	duration = frames / rate
+
+	time = dds.time_series(rate, duration)
+	band = ad9910_sweep_bandwidth(77, 1)
+	pulse_duration = 900/1000/1000
+
+	temporal_freq = (time / pulse_duration)*band - band/2
+	spectral_freq = torch.linspace(-rate/2, rate/2, frames)
+
+	# Prepare model signal
+	model_sweep = dds.sweep(time, -band/2, band/2, 0, pulse_duration)
+	spectrum_m = torch.fft.fft(model_sweep.flip(0).roll(1).conj())
+
+	# Load all captures
+	with open("20241001_071338_000_0000_003_000.ISE", "rb") as f:
+		captures_ref = StreamORDA(f).all_captures()
+
+	with open("20241001_072840_000_0000_003_000.ISE", "rb") as f:
+		captures_dut = StreamORDA(f).all_captures()
+
+	# We have a number of frequencies in the files
+	freqs_ref = set( [x.center_freq for x in captures_ref] )
+	freqs_dut = set( [x.center_freq for x in captures_dut] )
+
+	assert freqs_ref == freqs_dut
+
+	# Two of the frequencies are bugged and must be skipped
+	freq_set = freqs_ref - set( [0, 100*1000*1000] )
+
+	print(len(captures_ref))
+
+	# Filter captures to only include channel 1 and allowed frequencies
+	filter_fn = lambda x: x.center_freq in freq_set and x.channel_number == 1
+	captures_ref = list( filter(filter_fn, captures_ref) )
+	captures_dut = list( filter(filter_fn, captures_dut) )
+
+	def estimate_and_eliminate_time_delay_batch(signals):
+		spectra_s = torch.fft.fft(signals)
+		spectra_c = spectra_s * spectrum_m
+
+		# Time delay estimation
+		# V3 experimental
+		# + hack to work despite noise
+		f_shift = torch.fft.fftshift( (spectra_c.angle().diff() - 1) % -torch.pi + 1 )*frames/(2*torch.pi)
+		f_indices, = torch.where( (spectral_freq >= -band/4) * (spectral_freq <= band/4) )
+		f_shift2 = f_shift[:, f_indices]
+
+		sample_delay = -f_shift2.mean(1)
+		print("Delay V3:", sample_delay)
+
+		# Time delay elimination
+		# V1 exp(i 2pi k n/N)
+		spectra_e = spectra_s * torch.fft.fftshift( dds.timedelay(sample_delay[:, None], frames) )
+		signals = torch.fft.ifft(spectra_e)
+
+		return signals
+
+	zeroed_ref = estimate_and_eliminate_time_delay_batch( torch.vstack( [x.iq for x in captures_ref] ) )
+	zeroed_dut = estimate_and_eliminate_time_delay_batch( torch.vstack( [x.iq for x in captures_dut] ) )
+
+	for x, iq in zip(captures_ref, zeroed_ref):
+		x.iq = iq
+
+	for x, iq in zip(captures_dut, zeroed_dut):
+		x.iq = iq
+
+	indices = time <= pulse_duration
+
+	spectral = minmaxplot("Hz")
+	spectral.ytitle("dB")
+	spectral.xtitle("Частота")
+
+	#
+	# Do one at a time
+	#
+	for freq in sorted(freq_set):
+		filter_fn = lambda x: x.center_freq == freq
+		ref = filter(filter_fn, captures_ref)
+		dut = filter(filter_fn, captures_dut)
+
+		a = [x.iq for x in ref]
+		b = [x.iq for x in dut]
+		c = torch.vstack( [ v.abs() / u.abs() for u, v in zip(a, b) ] )
+
+		mampl = c.mean(0)
+		lower, _ = c.min(0)
+		upper, _ = c.max(0)
+
+		mampl = 10*torch.log10(mampl)
+		lower = 10*torch.log10(lower)
+		upper = 10*torch.log10(upper)
+
+		print(freq)
+
+		x = temporal_freq[indices] + freq
+		y = mampl[indices]
+
+		spectral.trace(x, y, error_band=(lower[indices], upper[indices]), name=f"{freq/1000/1000}MHz")
+
+	disp = page([spectral])
+	disp.show()
+	input()
