94d1b2980d12b99d41d0a79c8deb34d61e71031b
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-03-13 02:38:49 +0800

    Add amplitude_frequency_response_v2.py - working towards supporting the new capture+metadata format

---
+++ amplitude_frequency_response_v2.py
@@ -0,0 +1,308 @@
+
+from glob import glob
+import argparse
+import json
+
+import torch
+
+from deserializer import Model, Field
+
+from src.misc import ad9910_sweep_bandwidth, parse_numeric_expr, roll_lerp
+from src.delay import SpectralDelayEstimator
+from src.orda import StreamORDA
+import src.delay as delay
+import src.dds as dds
+
+
+class DDCSettingsV1(Model):
+	config_dir = Field(str)
+	samplerate = Field(str)
+	frames = Field(int)
+
+class SignalV1(Model):
+	tune = Field(str)
+	level = Field(str)
+	emit = Field(str)
+
+	est = None
+	time = None
+	model = None
+	delay = None
+	duration = None
+	temporal_freq = None
+	spectral_freq = None
+	temporal_indices = None
+
+	# Could we have model signal coalescing/materialization here?
+	# - Need samplerate/frames
+
+class DDCAndCalibratorV1(Model):
+	ddc = Field(DDCSettingsV1)
+	signals = Field([SignalV1])
+
+	# Modelling should really be here
+	# It needs both ddc and signals-
+	# and both are available here
+
+
+def parse_time_expr(expr, into="s"):
+	inv_factors = {
+		"s": 1,
+		"ms": 1000,
+		"us": 1000000,
+		"ns": 1000000000
+	}
+
+	value, unit = expr.split(" ")
+
+	if "." in value:
+		value = float(value)
+	else:
+		value = int(value)
+
+	inv_factor = inv_factors[ unit.lower() ] // inv_factors[ into.lower() ]
+
+	return value / inv_factor
+
+def parse_freq_expr(expr, into="hz"):
+	"""
+	Parse a frequency.
+
+	Examples:
+
+	>>> parse_freq_expr("150 MHz")
+	150000000
+	>>> parse_freq_expr("150.1 MHz")
+	150100000.0
+
+	Returns an int whenever possible.
+	"""
+
+	factors = {
+		"hz": 1,
+		"khz": 1000,
+		"mhz": 1000000,
+		"ghz": 1000000000
+	}
+
+	value, unit = expr.split(" ")
+
+	if "." in value:
+		value = float(value)
+	else:
+		value = int(value)
+
+	factor = factors[ unit.lower() ] // factors[ into.lower() ]
+
+	return value * factor
+
+#
+# Amplitude Frequency Response the 2nd:
+# Now systematized, streamlined, refined
+#
+# Two modes of operation:
+# - Without a reference
+# - Adjusted
+#
+# - Prep
+# 	- Take a directory
+#	- Deserialize the preset
+#	- Glob the .ISEs
+# - Prep 2
+#	- Model signals from the preset
+#	- Estimate and eliminate delay
+# - Frequency Response
+#	- Associate time with frequency
+#	- Retain, say, 0.9 of time
+#		- Previously we've been retaining an explicitly specified frequency band
+#			- This creates ambiguity for fixed frequency pulses
+#				- They do not really have a band
+#	- Into the point array
+#	- Sort it
+#	- Plot it
+#
+# Whenever the adjusted mode is used:
+# - Assert pulse set is the same
+# - That way point array X is absolutely surely the same
+# 	- Do not want to deal with resampling
+#		- Though it aint that hard
+#			- bins `x - x % 0.1`
+#			- new X np.unique(...)
+#			- gather x[b == 0.1]
+#				- for loop
+#				- stack
+# - 20log10 before plotting
+#
+#
+# Two mode architecture:
+# - A class FrequencyResponsePoints
+#	- Could have been a function too
+#		- But no access to preset
+#	- The constructor takes:
+#		- directory?
+#		- preset obj + captures?
+#	- x
+#	- y
+#
+
+
+class FrequencyResponsePointsV1:
+	"""
+	Application class to translate a folder of captures+metadata into frequency response points (x, y)
+	"""
+
+	def __init__(self, location):
+		with open(f"{location}/preset.json") as f:
+			obj = json.load(f)
+
+		preset = DDCAndCalibratorV1.deserialize(obj["ddc-and-calibrator-v1"])
+
+		captures = []
+
+		# No streaming, just load it all
+		# Will take up some RAM
+		#################################################################################################
+		for filename in sorted(glob(f"{location}/*.ISE")):
+			with open(filename, "rb") as f:
+				for capture in StreamORDA(f).captures:
+					if capture.center_freq == 0:
+						continue # DDC quirk: 0 Hz must be skipped
+
+					captures.append(capture)
+
+		chan_set = set([x.channel_number for x in captures])
+
+		# Trigger number rewrite
+		for i, capture in enumerate(captures):
+			capture.trigger_number = i // len(chan_set)
+
+		print("Loaded", len(captures), "captures")
+		print(len(chan_set), "channels active")
+
+		# Signal modelling
+		# Sowing and reaping
+		#################################################################################################
+		sysclk = parse_freq_expr("1 GHz")
+
+		frames = preset.ddc.frames
+		rate = parse_freq_expr(preset.ddc.samplerate)
+		capture_duration = frames / rate
+
+		# Modelling as DDC would have it
+		# TODO: Extract a set of parameters that matter, prep only that, propagate
+		for signal in preset.signals:
+			tokens = signal.emit.split(" ")
+
+			if tokens[0] == "sweep":
+				sweep, offset, offset_unit, duration, duration_unit, freq, freq_unit, a, b = tokens
+
+				delay = parse_time_expr(f"{offset} {offset_unit}")
+				duration = parse_time_expr(f"{duration} {duration_unit}")
+				center_frequency = parse_freq_expr(f"{freq} {freq_unit}")
+				a = int(a)
+				b = int(b)
+
+				#
+				# Prepare model signal
+				#
+				time = dds.time_series(rate, capture_duration)
+				band = ad9910_sweep_bandwidth(a, b, sysclk=sysclk)
+
+				model_sweep = dds.sweep(time, -band/2, band/2, 0, duration)
+				temporal_freq = (time / duration)*band - band/2
+				spectral_freq = torch.linspace(-rate/2, rate/2, frames)
+
+				#
+				# Prepare delay estimator
+				#
+				# We could have some kind of heuristic to decide on the estimator,
+				# for example having at least 100 bins occupied to pick SpectralDelayEstimator:
+ 				# bins_occupied = int( torch.unique(freqs - freqs % (rate/frames)).shape[0] )
+ 				#
+ 				# But lets have it simpler:
+				# - Sweeps have SpectralDelayEstimator - which assumes contiguous indices_est
+				# - Pulses have ConvDelayEstimator
+
+				# FIXME: Trim (retain) hardcoded
+				trim = 0.05
+
+				start = duration*trim
+				stop = duration*(1-trim)
+
+				temporal_indices = (time >= start) * (time < stop)
+
+				min_freq = temporal_freq[temporal_indices].min()
+				max_freq = temporal_freq[temporal_indices].max()
+
+				indices_est = (spectral_freq >= min_freq)*(spectral_freq < max_freq)
+				est = SpectralDelayEstimator(model_sweep, indices_est)
+
+				signal.est = est
+				signal.time = time
+				signal.model = model_sweep
+				signal.temporal_indices = temporal_indices
+				signal.temporal_freq = temporal_freq
+				signal.spectral_freq = spectral_freq
+				signal.duration = duration
+				signal.estimator = est
+			else:
+				assert 0
+
+
+		# Channel to points mapping
+		points = { chan: {"x": [], "y": []} for chan in chan_set }
+
+		# Onto the actual processing
+		# Delay elimination + amplitude + averaging
+		#################################################################################################
+		for i, signal in enumerate(preset.signals):
+			for channel in chan_set:
+				filter_fn = lambda x: x.trigger_number % len(preset.signals) == i and x.channel_number == channel
+
+				repeats = list(filter(filter_fn, captures))
+
+				assert all([x.center_freq == parse_freq_expr(signal.tune) for x in repeats])
+
+				a = [self.eliminate_time_delay(x.iq, signal.est) for x in repeats]
+				a = torch.vstack(a).abs()
+
+				mampl = a.mean(0)
+				lower, _ = a.min(0)
+				upper, _ = a.max(0)
+
+				indices = signal.temporal_indices
+
+				x = signal.temporal_freq[indices] + parse_freq_expr(signal.tune)
+				y = mampl[indices]
+
+				lower = lower[indices]
+				upper = upper[indices]
+
+				points[channel]["x"].append(x)
+				points[channel]["y"].append(y)
+
+		from src.display import minmaxplot, page
+
+		spectral = minmaxplot("Hz")
+
+		for chan, pts in points.items():
+			x = torch.hstack(pts["x"])
+			y = torch.hstack(pts["y"])
+
+			x, indices = torch.sort(x)
+			y = y[indices]
+
+			spectral.trace(x, y)
+
+		result = page([spectral])
+		result.show()
+
+	def eliminate_time_delay(self, iq, est):
+		sample_delay = est.estimate(iq)
+
+		# Use simple roll for now
+		return iq.roll( round(-sample_delay.item()) )
+
+
+
+FrequencyResponsePointsV1("/media/pop/2e7a55b1-cee8-4dd6-a513-4cd4c618a44e/calibrator_data_v1/calibrator_v1_2025-03-12T06_58_03+00_00")
---
+++ deserializer.py
@@ -0,0 +1,225 @@
+
+def flatten(lst):
+	"""
+	Flatten a list of lists
+	"""
+	for x in lst:
+		if isinstance(x, list):
+			for z in flatten(x):
+				yield z
+		else:
+			yield x
+
+def locations(lst):
+	"""
+	Flatten a list of lists and get (key, value) pairs
+	"""
+	for i, v in enumerate(lst):
+		if isinstance(v, list):
+			for suffix, value in locations(v):
+				yield (i,) + suffix, value
+		else:
+			yield (i,), v
+
+def structural_copy(lst):
+	"""
+	Produce an arrangement of lists of identical shape but filled with None
+	"""
+	return [structural_copy(x) if isinstance(x, list) else None for x in lst]
+
+def nested_set(lst, path, value):
+	for idx in path[:-1]:
+		lst = lst[idx]
+	lst[path[-1]] = value
+
+class Field:
+	def __init__(self, *args, required=True):
+		"""
+		Attribute descriptor.
+
+		Internally translates e.g. Field(int, float, str, [int], [float]) into:
+		FieldScope(int, float, str)
+		FieldScope([int])
+		FieldScope([float])
+
+		Scoping is to handle array variants.
+		"""
+
+		class FieldScope:
+			def __init__(self, path):
+				self.primitive = tuple()
+				self.composite = tuple()
+				self.path = path
+
+			def __repr__(self):
+				return "FieldScope(" + ", ".join(
+					[x.__name__ for x in self.primitive] +
+					[x.__name__ for x in self.composite]
+				) + ")"
+
+			def covers(self, path):
+				return len(self.path) - 1 == len(path)
+
+		scopes = { None: FieldScope( (0,) ) }
+
+		# Types unionized here
+		for k, arg in locations(args):
+			index = None if len(k) == 1 else k[:-1]
+			scope = scopes.get( index, FieldScope(k) )
+
+			if arg in [int, float, str, bool, type(None)]:
+				scope.primitive += (arg,)
+			else:
+				scope.composite += (arg,)
+
+			scopes[index] = scope
+
+		self.scopes = scopes
+		self.required = required
+
+class Model:
+	@classmethod
+	def fields(cls):
+		return dict(filter(
+			lambda kv: isinstance(kv[1], Field),
+			[ (k, getattr(cls, k)) for k in dir(cls)]
+		))
+
+	@classmethod
+	def required_fields(cls):
+		return dict(filter(
+			lambda kv: isinstance(kv[1], Field) and kv[1].required,
+			[ (k, getattr(cls, k)) for k in dir(cls)]
+		))
+
+	# to dict
+	def serialize(self):
+		pass
+
+	@classmethod
+	def trial_signatures(cls, obj, schemas):
+		"""
+		Picks a schema to deserialize a nested model
+		"""
+
+		present = set(obj)
+		extraneous = {}
+		lacking = {}
+
+		for schema in schemas:
+			pending = set(schema.required_fields())
+			allowed = set(schema.fields())
+
+			if pending - present:
+				lacking[schema] = pending - present
+				continue
+
+			if present - allowed:
+				extraneous[schema] = present - allowed
+				continue
+
+			return schema.deserialize(obj)
+
+		lacking = [f"Attempted to parse as {k.__name__} but lack required attrs {v}" for k, v in lacking.items()]
+		extraneous = [f"Attempted to parse as {k.__name__} but have extraneous attrs {v}" for k, v in extraneous.items()]
+
+		raise TypeError(f"\n" + "\n".join(lacking) + "\n".join(extraneous))
+
+	@classmethod
+	def interpret_list(cls, lst, scopes):
+		"""
+		List patterns can be basic:
+		[int, float, str, Model] => *:int|float|str|Model
+
+		Also multidimensional:
+		[[int]] => *.*:int
+
+		With alternatives:
+		[int], [float] => *:int, *:float
+
+		=== we DO NOT support those below ===
+
+		Mixed dimensionality:
+		[ int, [float] ] => *:int + *.*:float
+		[ int, [float], [[str]] ] => *:int + *.*:float + *.*.*:str
+
+		Those that need demotion:
+		[ int, [float], [str] ] => please rewrite as [ int, [float] ], [ int, [str] ]
+		[ int, [float, [str]], [bool, [Model]] ] => please rewrite as [ int, [float], [[str]] ], [ int, [bool], [[Model]] ]
+		"""
+
+		inapplicable = []
+
+		# We must:
+		# - [DONE IN Field()] Build a set of scopes
+		#	- May or may not cover paths to elements in array
+		# - [DONE HERE] Iterate through locations in a nested map() operation
+		#	- Array structure must be cloned to serve as output
+		#	- Lists never encountered
+		#	- Dicts handled through trial_signatures
+		#	- Primitive types handled through isinstance
+		for scope in scopes.values():
+			clone = structural_copy(lst)
+
+			try:
+				for k, v in locations(lst):
+					if not scope.covers(k):
+						raise TypeError("Dimensionality mismatch")
+
+					schemas = scope.composite
+					enforced = scope.primitive
+
+					if isinstance(v, dict) and schemas:
+						v = cls.trial_signatures(v, schemas)
+					else:
+						if not isinstance(v, enforced):
+							raise TypeError(f"array element {k}:{type(v).__name__} = {v} is not of any allowed type")
+
+					nested_set(clone, k, v)
+			except TypeError as e:
+				inapplicable.append(f"Tried to apply {scope} but failed with: {e}")
+				continue
+
+			# Reached here? success
+			return clone
+
+		raise TypeError("\n".join(inapplicable))
+
+	@classmethod
+	def deserialize(cls, obj):
+		pending = cls.required_fields()
+		allowed = cls.fields()
+		instance = cls()
+
+		# This will not run for nested
+		# Which is fine
+		if set(pending) - set(obj) != set(): raise TypeError(f"Required attr missing: {set(pending) - set(obj)}")
+		if set(obj) - set(allowed) != set(): raise TypeError(f"Extraneous attrs specified: {set(obj) - set(allowed)}")
+
+		# Attribute interpretation
+		# - Type enforcement for primitive types
+		# - Nesting for composite types
+		#	- Dicts
+		#	- Arrays
+		#		- Should be thought of as a kind of dict
+		#			- Should be flattened
+		#			- Indices are attributes
+		#				- Either covered by a rule
+		#				- Or not
+		for k, v in obj.items():
+			scopes = allowed.get(k).scopes
+			scope = scopes[None]
+			enforced = scope.primitive
+			schemas = scope.composite
+
+			if isinstance(v, dict) and schemas:
+				v = cls.trial_signatures(v, schemas)
+			elif isinstance(v, list):
+				v = cls.interpret_list(v, scopes)
+			else:
+				if not isinstance(v, enforced):
+					raise TypeError(f"{k}:{type(v).__name__} = {v} is not of any allowed type for {scope}")
+
+			setattr(instance, k, v)
+
+		return instance
