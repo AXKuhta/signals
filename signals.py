caa6bc001ee31806aec822b3b9aae74f8e334298
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-05-14 19:17:18 +0800

    Phase response comparison

--- signals.py
+++ signals.py
@@ -1,3 +1,3 @@
 from src import display, dds, ddc, orda, experiments, delay, misc
 
-experiments.calibrator_vs_vna.run_v2()
+experiments.calibrator_vs_vna.run_v3()
--- src/experiments/calibrator_vs_vna.py
+++ src/experiments/calibrator_vs_vna.py
@@ -16,6 +16,228 @@ import src.dds as dds
 from src.workflows.v1 import ModelSignalV1
 from src.schemas.v1 import *
 
+def run_v3():
+	class PhaseFrequencyResponsePointsV1:
+		"""
+		How to carry out phase measurements?
+		- DDC and the generator appear to trigger up to 200 ns apart
+			- Suspect the DDC's pipeline limits its triggering resolution
+			- The pipeline probably runs continuously
+			- Does not restart on trigger
+			- Trigger merely sets a "capture pending" flag
+			- Flag is effective next output of decimator
+		- What is the effect of delay on phase?
+			- May be thought of in terms of Dechirping
+				- Will produce up to 500 kHz / 900 us * 200 ns = 111.[1] Hz tone
+				- Will amount to 2*pi radian * 111.1 Hz * 900 us to deg = 36 deg travel
+				- Where there really should have been 0 deg travel
+			- May be thought of in terms of instanteneous frequency
+				- Zero Hz will not be affected by delay whatsoever
+				- For others, rad/s * delay extra angle introduced
+				- 2 pi radians 250 kHz 200 ns to deg = 18 deg
+		- Moreover, the DDC has a random phase offset problem
+			- The generator is known to produce repeatable waveforms
+			- Yet the captures have a random phase offset
+			- Meaning, the DDC's LO state different
+			- Either the result of triggering disparity
+			- Or the local oscillator is free-running
+			- Therefore it's impossible to tell the effect of DUT on phase
+			- Unless we can also tell the effect of DDC
+		- To fight this off two DDC channels must be used
+		- They are not 200 ns apart, they are coherent
+		- Treating one as incident
+		- The other as transmitted
+		- End up with a VNA
+		- Reference run
+			- Splitter	incident
+			- Thru		transmitted
+		- Test run
+			- Splitter	incident
+			- DUT		transmitted
+
+		So the math is:
+
+			z_n(t) = u_n(t) * v_n(t).conj
+
+		Where:
+
+			u_n(t)	transmitted
+			v_n(t)	incident
+
+		What passes for averaging is:
+
+			z(t) = sum_over_n( z_n(t) )
+			z(t) = z(t) / |z(t)| --- optional, if one wants 1.0 magnitude
+
+		Associate z(t) samples with freq
+		Extract a subset of z(t)
+
+		Then, for a referenced mode:
+
+			arg( z(t) * w(t).conj )
+		"""
+
+		def __init__(self, location, idx_a, idx_b, trim=0.05, radians=False):
+			with open(f"{location}/preset.json") as f:
+				obj = json.load(f)
+
+			preset = JsonDDCAndCalibratorV1.deserialize(obj["ddc-and-calibrator-v1"])
+
+			captures = []
+
+			# No streaming, just load it all
+			# Will take up some RAM
+			#################################################################################################
+			for filename in sorted(glob(f"{location}/*.ISE")):
+				with open(filename, "rb") as f:
+					for capture in StreamORDA(f).captures:
+						if capture.center_freq == 0:
+							continue # DDC quirk: 0 Hz must be skipped
+
+						captures.append(capture)
+
+			chan_set = set([x.channel_number for x in captures])
+
+			# Trigger number rewrite
+			for i, capture in enumerate(captures):
+				capture.trigger_number = i // len(chan_set)
+
+			print("Loaded", len(captures), "captures")
+			print("Active channels:", chan_set)
+
+			assert idx_a in chan_set, "Channel not available"
+			assert idx_b in chan_set, "Channel not available"
+
+			q_x = []
+			q_y = []
+
+			model_x = []
+			model_y = []
+
+			signals = []
+
+			for descriptor in preset.signals:
+				signals.append( ModelSignalV1(descriptor, preset.ddc) )
+
+			# Onto the actual processing
+			# Delay elimination + amplitude + averaging
+			#################################################################################################
+			for i, signal in enumerate(signals):
+				tune = parse_freq_expr(signal.descriptor.tune)
+
+				# Pulse cropping
+				start = signal.duration*trim + signal.delay
+				stop = signal.duration*(1-trim) + signal.delay
+
+				indices = (signal.time >= start) * (signal.time < stop)
+
+				# Model captures
+				x = signal.temporal_freq[indices] + tune
+				y = np.abs(signal.iq)[indices]
+
+				model_x.append(x)
+				model_y.append(y)
+
+				u_filter_fn = lambda x: x.trigger_number % len(signals) == i and x.channel_number == idx_a
+				v_filter_fn = lambda x: x.trigger_number % len(signals) == i and x.channel_number == idx_b
+
+				u_repeats = list(filter(u_filter_fn, captures))
+				v_repeats = list(filter(v_filter_fn, captures))
+
+				assert len(u_repeats) == len(v_repeats)
+				assert all([x.center_freq == tune for x in u_repeats])
+				assert all([x.center_freq == tune for x in v_repeats])
+
+				z_n = []
+
+				# Estimate delay once, for channel a
+				# DO NOT estimate delay individually
+				# Thay would defy the point
+				for u, v in zip(u_repeats, v_repeats):
+					delay = signal.est.estimate(u.iq)
+
+					u_ = np.roll(u.iq, -delay)
+					v_ = np.roll(v.iq, -delay)
+
+					z_n.append( u_ * v_.conj() )
+
+				z = np.sum(z_n, 0)
+
+				x = signal.temporal_freq[indices] + tune
+				y = z[indices]
+
+				q_x.append(x)
+				q_y.append(y)
+
+			# Second pass over the data to sort it - this deals with overlap
+			#################################################################################################
+			x = np.hstack(q_x)
+			y = np.hstack(q_y)
+
+			x, indices = np.sort(x), np.argsort(x)
+			y = y[indices]
+
+			self.q_x = x
+			self.q_y = y
+			self.chan_set = chan_set
+
+			self.model_x = np.hstack(model_x)
+			self.model_y = np.hstack(model_y)
+
+			self.idx_a = idx_a
+			self.idx_b = idx_b
+			self.radians = radians
+
+
+	mode = "narrowband"
+	#mode = "wideband"
+
+	if mode == "wideband":
+		#a = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T07_46_55+00_00", 3, 1) # Attenuator 5 dB
+		#a = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T07_56_09+00_00", 3, 1) # HPF
+		a = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T07_59_26+00_00", 3, 1) # LPF
+		b = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T07_44_00+00_00", 3, 1) # Thru
+	elif mode == "narrowband":
+		#a = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T08_07_26+00_00", 3, 1) # Attenuator 5 dB
+		#a = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T08_12_56+00_00", 3, 1) # HPF
+		a = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T08_15_13+00_00", 3, 1) # LPF
+		b = PhaseFrequencyResponsePointsV1("/media/pop/32/calibrator_data_v1/calibrator_v1_2025-04-15T08_05_05+00_00", 3, 1) # Thru
+	else:
+		assert 0
+
+	spectral = minmaxplot("Hz")
+	spectral.xtitle("Частота")
+	spectral.ytitle("Смещение фазы (°)")
+
+	factor = 180.0 / np.pi
+
+	adjusted = np.angle( b.q_y * a.q_y.conj() ) * factor
+
+	spectral.trace( b.q_x, adjusted, name=f"Калибратор")
+
+	fname_vna = "/media/pop/32/VNA/demoboard/lpf.s2p" # LPF
+	#fname_vna = "/media/pop/32/VNA/demoboard/hpf.s2p" # HPF
+	#fname_vna = "/media/pop/32/VNA/demoboard/minus_5db.s2p" # Attenuator 5 dB
+
+	with open(fname_vna, "rb") as f:
+		vna = S2PFile(f)
+
+	vna_x = np.array(vna.freqs)
+	vna_y = np.angle(vna.s21) * factor
+
+	if mode == "narrowband":
+		indices = np.where( (vna_x >= 154*1000*1000)*(vna_x <= 162*1000*1000) )
+		vna_x = vna_x[indices]
+		vna_y = vna_y[indices]
+
+	spectral.trace(vna_x, vna_y, name="VNA")
+
+	#err_y = np.interp(vna_x, x, y) - np.array(vna_y)
+	#spectral.trace(vna_x, err_y, name="Calibrator error", hidden=True)
+
+	result = page([spectral])
+	result.show()
+
 def run_v2():
 	class Mode(Enum):
 		RAW = auto()
